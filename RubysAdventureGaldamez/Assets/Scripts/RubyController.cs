d1db05bcee4c7ff8017ab1fef2b603350e714028
MechelleGG <752429@student.dusd.net>
2022-12-05 09:55:49 -0800

    I did more
    I am writing the animation script's

--- RubysAdventureGaldamez/Assets/Scripts/RubyController.cs
+++ RubysAdventureGaldamez/Assets/Scripts/RubyController.cs
@@ -19,10 +19,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator:
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         currentHealth = maxHealth;
     }
 
@@ -31,6 +36,9 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.App)
 
         if (isInvincible)
         {
